d3a1d27121f2d58db0dcbc78f8aa4f039e57e536
Sagi Vaknin <sagi8520@gmail.com>
2023-12-07 20:06:33 +0200

    .

--- src/index.js
+++ src/index.js
@@ -2,6 +2,8 @@
 import { Cartesian3, createOsmBuildingsAsync, Ion, Math as CesiumMath, Terrain, Viewer } from 'cesium';
 import "cesium/Build/Cesium/Widgets/widgets.css";
 
+const zoomedOut=200;
+
 window.CESIUM_BASE_URL = '/static/Cesium/';
 // Your access token can be found at: https://ion.cesium.com/tokens.
 // This is the default access token from your ion account
@@ -24,21 +26,52 @@ function cesiumCoordsToPixel(longitude, latitude, globeWidth, globeHeight) {
   return { x, y };
 }
 
+function pixelToCesiumCoords(x, y, globeWidth, globeHeight) {
+  const longitude = (x / globeWidth) * 360 - 180;
+  const latitude = 90 - (y / globeHeight) * 180;
+  return { longitude, latitude };
+}
+
+function flyToLocationInCesium(longitude, latitude) {
+  viewer.camera.flyTo({
+    destination: Cartesian3.fromDegrees(longitude, latitude,zoomedOut),
+  });
+}
+
 function moveArrowToCesiumCameraPosition() {
   const cameraPositionCartographic = viewer.camera.positionCartographic;
-  
+
   const longitude = CesiumMath.toDegrees(cameraPositionCartographic.longitude);
   const latitude = CesiumMath.toDegrees(cameraPositionCartographic.latitude);
-  
+
   const globe = document.getElementById('globe');
   const globeWidth = globe.clientWidth;
   const globeHeight = globe.clientHeight;
-  
+
   const pixelCoords = cesiumCoordsToPixel(longitude, latitude, globeWidth, globeHeight);
-  
+
   const arrow = document.getElementById('arrow');
   arrow.style.left = `${pixelCoords.x}px`;
   arrow.style.top = `${pixelCoords.y}px`;
 }
 
 viewer.camera.moveEnd.addEventListener(moveArrowToCesiumCameraPosition);
+document.getElementById('globe').addEventListener('click', function (event) {
+  const globeRect = this.getBoundingClientRect();
+  const x = event.clientX - globeRect.left; // x position within the element.
+  const y = event.clientY - globeRect.top;  // y position within the element.
+
+  const globeWidth = this.clientWidth;
+  const globeHeight = this.clientHeight;
+
+  // Convert clicked pixel coordinates to geographic coordinates
+  const { longitude, latitude } = pixelToCesiumCoords(x, y, globeWidth, globeHeight);
+
+  // Move the arrow to the clicked location
+  const arrow = document.getElementById('arrow');
+  arrow.style.left = `${x}px`;
+  arrow.style.top = `${y}px`;
+
+  // Move the Cesium camera to the clicked location
+  flyToLocationInCesium(longitude, latitude);
+});
